ff73447c198e6b1eee0df85d1552f09a56badcaa
Petter Hetland <Pehe@nmbu.no>
2019-11-27 00:10:37 +0100

    Completed the project.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,49 @@
 <template>
-  <div id="app" class ="min-h-screen w-screen bg-gray-200 flex items-center justify-center">
-    <ul class="w-full max-w-md">
-    <li v-for="user in users"
-        :key="user.id"
-        class="p-4 mb-3 flex justify-between items-center bg-white shadow rounded-lg cursor-move">
-      {{user.name}}
-    </li>
-  </ul>
+  <div
+    id="app"
+    class="min-h-screen w-screen bg-gray-200 flex flex-col pt-20 justify-center items-center md:items-start md:flex-row"
+  >
+    <div class="w-full max-w-md text-center px-3">
+      <p class="mb-2 text-gray-700 font-semibold font-sans tracking-wide">List 1</p>
+      <draggable
+        tag="ul"
+        group="all-users"
+        class="draggable-list"
+        ghost-class="moving-card"
+        filter=".action-button"
+        :list="users"
+        :animation="200"
+      >
+        <user-card
+          v-for="user in users"
+          :user="user"
+          :key="user.id"
+          @on-edit="onEdit"
+          @on-delete="onDelete"
+        ></user-card>
+      </draggable>
+    </div>
+
+    <div class="w-full max-w-md md:ml-6 text-center px-3">
+      <p class="mb-2 text-gray-700 font-semibold font-sans tracking-wide">List 2</p>
+      <draggable
+        tag="ul"
+        group="all-users"
+        class="draggable-list"
+        ghost-class="moving-card"
+        filter=".action-button"
+        :list="newUsers"
+        :animation="200"
+      >
+        <user-card
+          v-for="user in newUsers"
+          :user="user"
+          :key="user.id"
+          @on-edit="onEdit"
+          @on-delete="onDelete"
+        ></user-card>
+      </draggable>
+    </div>
   </div>
 </template>
 
@@ -27,29 +64,29 @@ export default {
       users: [
         {
           id: 1,
-          name: "Adrian Schubert",
+          name: "Max Manus",
           avatar:
             "https://pickaface.net/gallery/avatar/unr_sample_161118_2054_ynlrg.png"
         },
         {
           id: 2,
-          name: "Violet Gates",
+          name: "Ingebjørg Skoghaug",
           avatar: "https://pickaface.net/gallery/avatar/freud51c8b3f65e7dc.png"
         },
         {
           id: 3,
-          name: "Steve Jobs",
+          name: "Einar Skinnarland",
           avatar: "https://pickaface.net/gallery/avatar/Opi51c74d0125fd4.png"
         },
         {
           id: 4,
-          name: "Yassine Smith",
+          name: "'Tante'",
           avatar:
             "https://pickaface.net/gallery/avatar/unr_yassine_191124_2012_3gngr.png"
         },
         {
           id: 5,
-          name: "Senior Saez",
+          name: "Gunnar Sønsteby",
           avatar:
             "https://pickaface.net/gallery/avatar/elmedinilla541c03412955c.png"
         }
@@ -57,30 +94,30 @@ export default {
       newUsers: [
         {
           id: 6,
-          name: "Steve Jobs",
+          name: "Joachim Rønneberg",
           avatar: "https://pickaface.net/gallery/avatar/Opi51c74d0125fd4.png"
         },
         {
           id: 7,
-          name: "Yassine Smith",
+          name: "Gregers Gram",
           avatar:
             "https://pickaface.net/gallery/avatar/unr_yassine_191124_2012_3gngr.png"
         },
         {
           id: 8,
-          name: "Senior Saez",
+          name: "Martin Linge",
           avatar:
             "https://pickaface.net/gallery/avatar/elmedinilla541c03412955c.png"
         },
         {
           id: 9,
-          name: "Adrian Schubert",
+          name: "Claus Helberg",
           avatar:
             "https://pickaface.net/gallery/avatar/unr_sample_161118_2054_ynlrg.png"
         },
         {
           id: 10,
-          name: "Violet Gates",
+          name: "Sverre Granlund",
           avatar: "https://pickaface.net/gallery/avatar/freud51c8b3f65e7dc.png"
         }
       ]
@@ -97,14 +134,15 @@ export default {
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style>
+.draggable-list {
+  min-height: 100px;
+}
+/* Unfortunately @apply cannot be setup in codesandbox,
+but you'd use "@apply border opacity-50 border-blue-500 bg-gray-200" here */
+.moving-card {
+  opacity: 0.5;
+  background: #F7FAFC;
+  border: 1px solid #4299e1;
 }
 </style>
-
